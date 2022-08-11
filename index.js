3ef45fa19d4260fa5992c92add92fab35d03251f
MLTurner <mlturne3@ncsu.edu>
2022-08-10 20:56:21 -0400

    adding hashed passwords and compare passwords

--- index.js
+++ index.js
@@ -7,27 +7,30 @@ const readlineSync = require('readline-sync')
 // We'll keep a global object to store usernames and password hashes
 let globalStore = {}
 
-
-
 /*
 * SOLUTION CODE FOR BCRYPT FUNCTIONS
 */
 
+
+
 // function for checking a password
 checkPassword = async (username, plaintextPassword) => {
     // TODO: Make sure to delete this console.log once you're done implementing the function!
-    console.log('\n Uh-oh, checkPassword is not yet implemented. 😢')
+
     // Ensure global store contains the user 
     // (this is a quick way to check if an object contains a key)
     if (globalStore[username]) {
-        // TODO: Use bcrypt's compare methof to compare a plaintext password to a password hash
-
+        
+        // TODO: Use bcrypt's compare method to compare a plaintext password to a password hash
+        let result = await bcrypt.compare(plaintextPassword, globalStore[username])
         // TODO: The result variable is a boolean. True means the user was valid. Take action accordingly.
         if (result) {
             // TODO: Display message for valid credentials
+            console.log(`Welcome back ${username}!`)
         }
         else {
             // TODO: Display message for invalid credentials
+            console.log(`GET OUT`)
         }
     }
     else {
@@ -38,13 +41,12 @@ checkPassword = async (username, plaintextPassword) => {
 
 hashPassword = async (username, password) => {
     // TODO: Make sure to delete this console.log once you're done implementing the function!
-    console.log('\nUh-oh, hashPassword is not yet implemented. 😢')
-
     // TODO: Make the password hash using bcrypt
-
+let hashPassword = await bcrypt.hash(password, 12)
     // TODO: Add the user and password hash to the global store object
-
+    globalStore[username] = hashPassword
     // TODO: Print a status update including the username and password hash
+    console.log(`user ${username} was added; the hashed password is ${hashPassword}`)
 }
 
 
